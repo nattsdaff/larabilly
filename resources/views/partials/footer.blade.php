a88616c80ffe476430649ff5e94cdd392e1c751f
Capu <capu@Admins-iMac.local>
2018-12-15 19:11:08 -0300

    arreglos footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,11 +1,11 @@
 <footer>
     <div class="container">
-        
+        {{-- <div class="copy"><p>2018&copy; Todos los derechos reservados.</p></div> --}}
         <!--NEWSLETTER-->
         <section class="newsletter">
             <div class="container-newsletter">
                 <h3 class="alt-title">Suscribite al newsletter</h3>
-                <p class="newsletter-info">Recibí nuestras ofertas y novedades directamente en tu e-mail.</p>
+                {{-- <p class="newsletter-info">Recibí nuestras ofertas y novedades directamente en tu e-mail.</p> --}}
                 <form action="#" method="post" class="form">
                     <div class="form-item">
                         <input type="email" name="email" placeholder="Ingrese su e-mail" required class="form-field" value="email">
@@ -15,18 +15,17 @@
             </div>
         </section>
         <!--END OF NEWSLETTER-->
-
-        <div class="alt-menu">
-            <img src="\assets/img/logo-white.png" alt="Mobili Logo">
-            <ul>
-                <li><a href="#">Servicio al cliente</a></li>
-                <li><a href="#">Nuestra empresa</a></li>
-                <li><a href="/faq">Preguntas frecuentes</a></li>
-                <li><a href="#">Envios</a></li>
-                <li><a href="#">Cambios y devoluciones</a></li>
-            </ul>
-            <br>
-            <div class="copy"><p>2018&copy; Todos los derechos reservados.</p></div>
+        <div><img src="\assets/img/logo-white.png" alt="Mobili Logo">
+            <div class="alt-menu">
+                    <ul>
+                        <li><a href="#">Servicio al cliente</a></li>
+                        <li><a href="#">Nuestra empresa</a></li>
+                        <li><a href="/faq">Preguntas frecuentes</a></li>
+                        <li><a href="#">Envios</a></li>
+                        <li><a href="#">Cambios y devoluciones</a></li>
+                    </ul>
+                </div>
+                <p class="copy">2018&copy; Todos los derechos reservados.</p>
         </div>
     </div>
 </footer>
